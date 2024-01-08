cfcd86790ad4cf546e09156bc67481118c2effa1
Soulgrimm <Dante94_94@bk.ru>
2024-01-08 19:07:56 +0300

    HW21.1

--- catalog/models.py
+++ catalog/models.py
@@ -41,7 +41,7 @@ class Blog(models.Model):
     image = models.ImageField(upload_to='product/', verbose_name='Изображение', **NULLABLE)
     date_of_creation = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
     publication_sign = models.BooleanField(default=True, verbose_name='Опубликовано')
-    views_count = models.IntegerField(default=0, verbose_name='Просмотры')
+    views_count = models.PositiveIntegerField(default=0, verbose_name='Просмотры')
 
     def __str__(self):
         return self.title
